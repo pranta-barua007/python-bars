8aadb0a92c46882079d55e9c8dcaa869f00707f0
pranta-barua007 <prantabarua247@gmail.com>
2021-03-06 14:25:38 +0600

    add student teacher and project through form in  add/ route

--- mysite/graphbars/views.py
+++ mysite/graphbars/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from . models import Department, Student, Teacher, Project
 
 # Create your views here.
@@ -38,3 +39,38 @@ def index(response):
         "teProj":teProj,
         "meProj":meProj
     })
+
+
+def add(response):
+    getDep = Department.objects
+
+    if response.method == 'POST':
+        print(response.POST)
+        getFormDep = response.POST.get('dep')
+
+        if response.POST.get('addInfo') == 'Add Student':
+            getStdntName = response.POST.get('studentName')
+            getStdntGender = response.POST.get('studentGender')
+
+            if len(getStdntName) > 2:
+                addStdntOnDep = Department.objects.get(name=getFormDep)
+                addStdntOnDep.student_set.create(name=getStdntName, sex=getStdntGender)
+                return HttpResponseRedirect("/add")
+
+        if response.POST.get('addInfo') == 'Add Teacher':
+            getTeacherName = response.POST.get('teacherName')
+
+            if len(getTeacherName) > 2:
+                addTeacherOnDep = Department.objects.get(name=getFormDep)
+                addTeacherOnDep.teacher_set.create(name=getTeacherName)
+                return HttpResponseRedirect("/add")
+
+        if response.POST.get('addInfo') == 'Add Project':
+            getProjectName = response.POST.get('projectName')
+
+            if len(getProjectName) > 2:
+                addProjectOnDep = Department.objects.get(name=getFormDep)
+                addProjectOnDep.project_set.create(name=getProjectName)
+                return HttpResponseRedirect("/add")
+
+    return render(response, 'graphbars/add.html', {"getDep": getDep})
